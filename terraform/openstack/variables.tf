e66c6389cda6864ba5076b190d745220bb90b4fd
gravitasse <gravitasse@users.noreply.github.com>
2025-10-31 22:33:57 -0700

    docs: add example terraform/ansible configs and generated files

---
+++ terraform/openstack/variables.tf
@@ -0,0 +1,27 @@
+variable "os_auth_url" {
+  type = string
+}
+
+variable "os_tenant" {
+  type = string
+}
+
+variable "os_user" {
+  type = string
+}
+
+variable "os_pass" {
+  type = string
+}
+
+variable "control_count" {
+  type = number
+}
+
+variable "nvidia_gpu_count" {
+  type = number
+}
+
+variable "intel_gpu_count" {
+  type = number
+}
